cfb230006da0a302eb8626603cae0f816f20d8c6
Vishal Arasu <133478305+Viz1303@users.noreply.github.com>
2025-04-28 18:57:21 -0400

    Update streamlit_NLP.py

--- streamlit_NLP.py
+++ streamlit_NLP.py
@@ -5,7 +5,7 @@
 
 import streamlit as st
 st.set_page_config(
-    page_title="NLP Retail Sentiment Dashboard",
+    page_title="NLP Amazon Food Reviews Sentiment Dashboard",
     page_icon="🛒",
     layout="wide",
     initial_sidebar_state="expanded"
